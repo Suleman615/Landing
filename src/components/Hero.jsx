ed3f041a4b95d4ebc3665c7b32945ab7b8e21582
Suleman615 <ghlmsuleman@gmail.com>
2024-06-07 17:17:28 +0500

    text update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,15 @@
 const Hero = () => {
     return (
         <>
-            <div className="flex flex-col gap-4 justify-center items-center pb-10 pt-28  px-2  bg-[#F3F4F6]">
-                <h5 className="text-[48px] text-center text-[#585F6E]">שינוי משפטי<br /> <span className='text-[#2A313F]'> מתאמן עם AI</span></h5>
-                <p className='text-center text-[#757C89]'>גלה ניהול, ניתוח וסיוע של מסמכים משפטיים חלקים.</p>
-                <div dir='rtl' className="flex  gap-4  font-semibold">
+            <div className="flex flex-col gap-4 justify-center items-center pb-10 pt-28  px-2  bg-[#F3F4F6] h-screen">
+                <h5 className="text-[48px] text-center text-[#2A313F]">מערכת AI לשכנוע בעולם המשפט</h5>
+                <p className='text-center text-[#757C89] max-w-[50%] m'>גלה חולשות וחוזקות בכתבי טענות והסכמים, נתח וסכם רעיונות כהרף עין וכתוב מחדש בעברית מעולה טיעונים משפטיים מנצחים לבתי המשפט.</p>
+                <div dir='rtl' className="flex my-10  gap-4  font-semibold">
 
-                    <button type="button" className=" bg-[#2E3747] text-white rounded-lg px-4 py-0.5 hover:bg-[#1d2129]">להתחבר</button>
-                    <button type="button" className="bg-white text-[#2E3747] rounded-lg px-4 py-0.5 border-2 shadow-md border-[#eeeff1] hover:border-[#b2b5b8] hover:shadow-lg">להתחבר</button>
+                    <button type="button" className=" bg-[#2E3747] text-white rounded-lg px-4 py-0.5 hover:bg-[#1d2129]">נסה ללא תשלום</button>
+                    <button type="button" className="bg-white text-[#2E3747] rounded-lg px-4 py-0.5 border-2 shadow-md border-[#eeeff1] hover:border-[#b2b5b8] hover:shadow-lg">התחבר</button>
                 </div>
-                <p className='text-center text-[#757C89]'>גלה ניהול, ניתוח וסיוע של מסמכים משפטיים חלקים.</p>
+                <p className='text-center text-[#757C89]'>חמורבי עובד עם ארגוני המשפט והמשרדים הטובים בישראל</p>
 
             </div>
         </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,22 +9,23 @@ const Navbar = () => {
     return (
         <>
             <div dir="rtl" className="flex justify-between  items-center h-16  px-2 md:px-10 border-b bg-[#F3F4F6] fixed w-full ">
-                <h2 className="text-[24px] font-bold">עורך דין AI</h2>
+                <h2 className="text-[24px] font-bold">@חמורבי.</h2>
                 <Menu onClick={() => setMobile(!mobile)} className="visible md:hidden" />
 
 
                 {/* wied screen */}
                 <div dir="rtl" className=" h-full hidden sm:flex  pl-8 ml-6 font-semibold items-center">
-                    <a className="cursor-pointer h-full flex justify-center items-center px-2 gradient-background  " >מאפיינים</a>
-                    <a className="cursor-pointer h-full flex justify-center items-center px-2 gradient-background" >תמחור</a>
+                    <a className="cursor-pointer h-full flex justify-center items-center px-2 gradient-background  " >יישומים</a>
+                    <a className="cursor-pointer h-full flex justify-center items-center px-2 gradient-background" >שאלות ותשובות</a>
+                    <a className="cursor-pointer  h-full flex justify-center items-center px-2 gradient-background" >תוכניות</a>
+                    <a className="cursor-pointer  h-full flex justify-center items-center px-2 gradient-background" >בלוג</a>
                     <a className="cursor-pointer  h-full flex justify-center items-center px-2 gradient-background" >תמיכה</a>
-                    <a className="cursor-pointer  h-full flex justify-center items-center px-2 gradient-background" >למה לנו?</a>
 
                 </div>
 
                 <div dir="rtl" className="hidden md:flex   gap-4 font-semibold">
-                    <button type="button" className=" bg-[#2E3747] text-white rounded-lg px-4 py-0.5 hover:bg-[#1d2129]">להתחבר</button>
-                    <button type="button" className="bg-white text-[#2E3747] rounded-lg px-4 py-0.5  border-2 shadow-md border-[#eeeff1] hover:border-[#b2b5b8] hover:shadow-lg">להתחבר</button>
+                    <button type="button" className=" bg-[#2E3747] text-white rounded-lg px-4 py-0.5 hover:bg-[#1d2129]">התחבר</button>
+                    <button type="button" className="bg-white text-[#2E3747] rounded-lg px-4 py-0.5  border-2 shadow-md border-[#eeeff1] hover:border-[#b2b5b8] hover:shadow-lg">נסה ללא תשלום</button>
 
                 </div>
 
@@ -33,15 +34,16 @@ const Navbar = () => {
                 {mobile &&
                     <div className="absolute md:hidden top-16 left-0 ps-5 gap-4 w-full flex flex-col items-start bg-[#F3F4F6] bg-opacity-90 py-4">
                         <div className="flex flex-col gap-4  font-semibold items-start">
-                            <a onClick={() => setMobile(!mobile)} className="cursor-pointer" >מאפיינים</a>
-                            <a onClick={() => setMobile(!mobile)} className="cursor-pointer" >תמחור</a>
+                            <a onClick={() => setMobile(!mobile)} className="cursor-pointer" >יישומים</a>
+                            <a onClick={() => setMobile(!mobile)} className="cursor-pointer" >שאלות ותשובות</a>
+                            <a onClick={() => setMobile(!mobile)} className="cursor-pointer" >תוכניות</a>
+                            <a onClick={() => setMobile(!mobile)} className="cursor-pointer" >בלוג</a>
                             <a onClick={() => setMobile(!mobile)} className="cursor-pointer" >תמיכה</a>
-                            <a onClick={() => setMobile(!mobile)} className="cursor-pointer" >למה לנו?</a>
 
                         </div>
                         <div className="flex flex-col gap-4 font-semibold">
-                        <button type="button" className=" bg-[#2E3747] text-white rounded-lg px-4 py-0.5 hover:bg-[#1d2129]">להתחבר</button>
-                    <button type="button" className="bg-white text-[#2E3747] rounded-lg px-4 py-0.5  border-2 shadow-md border-[#eeeff1] hover:border-[#b2b5b8] hover:shadow-lg">להתחבר</button>
+                        <button type="button" className=" bg-[#2E3747] text-white rounded-lg px-4 py-0.5 hover:bg-[#1d2129]">התחבר</button>
+                    <button type="button" className="bg-white text-[#2E3747] rounded-lg px-4 py-0.5  border-2 shadow-md border-[#eeeff1] hover:border-[#b2b5b8] hover:shadow-lg">נסה ללא תשלום</button>
 
                         </div>
                     </div>
