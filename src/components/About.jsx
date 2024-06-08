7e276dffa3ea0885e7f078d5c67b6a651226dce0
Suleman615 <ghlmsuleman@gmail.com>
2024-06-09 01:51:39 +0500

    text update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
                 <h5 className="text-[48px] text-center  text-[#585F6E]">איך להשתמש בחמורבי.</h5>
                 <p className=' text-[#757C89] text-center'>סרטון לימודי המבהיר כיצד להשתמש בחמורבי על מנת לשפר את ביצועיך כמשפטן ועורך דין.</p>
             </div>
-            <div className='p-4 w-full my-20 h-[100vh]  '>
+            <div className=' w-full mt-20 h-[100vh]  '>
                 <div className='border p-1 rounded-lg w-[80%] md:w-3/4 h-[90%] mx-auto'>
                     <iframe className='w-full h-full rounded-lg' src="https://www.youtube.com/embed/RwU7YY6emYc" title="Placeholder video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 </div>
